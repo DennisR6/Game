12cbe9c5cf3cc45132ddc4d3e1b49995bec588b2
Eugen <eugen252009@gmail.com>
2026-02-08 20:33:33 +0100

    implemented new Physics and tests

--- src/physics/physics.ts
+++ src/physics/physics.ts
@@ -1,9 +1,24 @@
+export { defaultPhysics } from "./defaultPhysics.ts"
 export interface Vector2D {
 	x: number;
 	y: number;
 }
 export interface PhysicsStrategy {
-	calculateBounce(vel: Vector2D, normal: Vector2D): Vector2D | null;
+	calculateBounce(vel: Vector2D, normal: Vector2D): Vector2D;
+	add(a: Vector2D, b: Vector2D): Vector2D
+	sub(a: Vector2D, b: Vector2D): Vector2D
+	mult(a: Vector2D, b: number): Vector2D
+	dot(a: Vector2D, b: Vector2D): number
+	magSq(v: Vector2D): number
+	mag(v: Vector2D): number
+	normalize(v: Vector2D): Vector2D
+	dist(a: Vector2D, b: Vector2D): number;
+	distSq(a: Vector2D, b: Vector2D): number;
+	clamp(val: number, min: number, max: number): number
+	//Collisions
+	checkCollisionCircles(pos1: Vector2D, r1: number, pos2: Vector2D, r2: number): boolean;
+	checkCollisionRects(pos1: Vector2D, w1: number, h1: number, pos2: Vector2D, w2: number, h2: number): boolean;
+	checkCollisionCircleRect(circlePos: Vector2D, r: number, rectPos: Vector2D, w: number, h: number): boolean;
 }
 export type IPhysics = IPhysicsCircle | IPhysicsRectangle
 export interface IPhysicsCircle {
@@ -25,4 +40,3 @@ export interface IPhysicsRectangle {
 
 
 
-export { p5Physics } from "./p5Physics.ts"
--- src/structures/structureRectangle.ts
+++ src/structures/structureRectangle.ts
@@ -1,22 +1,43 @@
 import type { RenderContext } from "../engine/RenderContext"
+import type { IPhysicsRectangle, Vector2D } from "../physics/physics"
 import type { IStructure } from "./structures"
 
-export class StructureRectangle implements IStructure {
+export class StructureRectangle implements IStructure, IPhysicsRectangle {
 	x: number
 	y: number
 	w: number
 	h: number
 	color: string
+	shape: "rectangle"
+	vel: Vector2D
+	bounce: number
 	constructor(x: number, y: number, w: number, h: number, color: string) {
 		this.x = x
 		this.y = y
 		this.w = w
 		this.h = h
 		this.color = color || "green"
+		this.shape = "rectangle"
+		this.vel = { x: 0, y: 0 }
+		this.bounce = 0
 	}
 	draw(ctx: RenderContext) {
 		ctx.setFillColor(this.color)
 		ctx.drawRect(this.x, this.y, this.w, this.h)
 	}
+	getBounceFactor(): number {
+		return this.bounce
+	}
+	getBounds(): { width: number; height: number } {
+		return { width: this.w, height: this.h }
+	}
+	getPos(): Vector2D {
+		return { x: this.x, y: this.y }
+	}
+	getVelocity(): Vector2D {
+		return this.vel
+	}
+	onCollision(_impact: { newPos: Vector2D; newVel: Vector2D }): void {
+		// NOTE: Do Noting - see StructureCircle
+	}
 }
-
